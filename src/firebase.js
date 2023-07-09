96828a06a30eb3becb8b86f7ccab0177735d47f2
Ajay Kumar <ajaykumarkv0317@gmail.com>
2023-07-09 12:28:10 +0530

    commeeeee

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import {GoogleAuthProvider, getAuth,signInWithPopup,signInWithEmailAndPassword,  createUserWithEmailAndPassword, sendPasswordResetEmail, signOut} from "firebase/auth";
+import { getAuth } from "firebase/auth";
 const firebaseConfig = {
     apiKey: "AIzaSyCtGsA1ZsO3Ty9ARLgvmJa1hJLDeRPbLG8",
     authDomain: "libraryproject-142cb.firebaseapp.com",
